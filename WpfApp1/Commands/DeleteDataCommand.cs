3c80508289f222855ee522652d8694d3feeb570f
Artem Mironov <progiedo@gmail.com>
2022-05-24 22:19:26 +0300

    Add project files.

---
+++ WpfApp1/Commands/DeleteDataCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using WpfApp1.Model;
+using WpfApp1.ViewModels;
+
+namespace WpfApp1.Commands
+{
+    public class DeleteDataCommand : Commands
+    {
+        private DataWindowViewModel _viewModel;
+
+        public DeleteDataCommand(DataWindowViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public override void Execute(object parameter)
+        {
+            if (_viewModel.SelectedEmployee == null)
+            {
+                return;
+            }
+
+            int employeeId = (_viewModel.SelectedEmployee as Employee).Id;
+
+            using (var DbContext = new DatabaseEntities())
+            {
+                var employee = DbContext.Employees
+                    .Where(e => e.Id == employeeId)
+                    .Single();
+
+                DbContext.Employees.Remove(employee);
+                DbContext.SaveChanges();
+
+                _viewModel.Employees.Remove(employee);
+                (parameter as ItemCollection).Refresh();
+            }
+
+            UpdateEmployees();
+            (parameter as ItemCollection).Refresh();
+        }
+
+        private void UpdateEmployees()
+        {
+            using (var DbContext = new DatabaseEntities())
+            {
+                _viewModel.Employees = DbContext.Employees
+                    .ToList();
+            }
+        }
+    }
+}
---
+++ WpfApp1/ViewModels/DataWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp1.Commands;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModels
+{
+    public class DataWindowViewModel : ViewModel
+    {
+        private List<Employee> employees;
+        private object selectedEmployee;
+
+        public ICommand DeleteData => new DeleteDataCommand(this);
+
+        public DataWindowViewModel()
+        {
+            using (var DbContext = new DatabaseEntities())
+            {
+                employees = DbContext.Employees
+                    .ToList();
+            }
+        }
+
+        public List<Employee> Employees
+        {
+            get => employees;
+            set => Set(ref employees, value, nameof(Employees));
+        }
+
+        public object SelectedEmployee
+        {
+            get => selectedEmployee;
+            set => Set(ref selectedEmployee, value, nameof(SelectedEmployee));
+        }
+    }
+}
